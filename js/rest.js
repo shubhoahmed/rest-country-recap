86cecef2865ee4b1d4f6f5e5a4594b1876757cb6
shubhoahmed <shubhoahmed03@gmail.com>
2022-09-19 00:12:30 +0600

    css added

---
+++ js/rest.js
@@ -0,0 +1,23 @@
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => displayCountries(data));
+}
+const displayCountries = countries => {
+    console.log(countries);
+    const allCountriesHTML = countries.map(country => getCountryHTML(country));
+    // console.log(country);
+    const container = document.getElementById('countries');
+    container.innerHTML = allCountriesHTML.join('');
+}
+
+const getCountryHTML = country => {
+    return `
+        <div class = "country">
+            <h2> ${country.name.common}</h2>
+            <img src="${country.flags.png}">
+        </div>
+    `
+}
+
+loadCountries();
